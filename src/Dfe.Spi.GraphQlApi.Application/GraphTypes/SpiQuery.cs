201214b479a632c428d5cfa83434c96946f29f34
Simon Bull <simon.bull.84@googlemail.com>
2020-02-10 10:08:15 +0000

    Add extra query args to learningProvider

--- src/Dfe.Spi.GraphQlApi.Application/GraphTypes/SpiQuery.cs
+++ src/Dfe.Spi.GraphQlApi.Application/GraphTypes/SpiQuery.cs
@@ -11,9 +11,18 @@ namespace Dfe.Spi.GraphQlApi.Application.GraphTypes
         {
             Field<LearningProviderType>("learningProvider",
                 resolve: learningProviderResolver.ResolveAsync,
-                arguments: new QueryArguments(
-                    new QueryArgument<IntGraphType> {Name = "urn"}));
-            
+                arguments: new QueryArguments(new QueryArgument[]
+                {
+                    new QueryArgument<IntGraphType> {Name = "urn"},
+                    new QueryArgument<IntGraphType> {Name = "ukprn"},
+                    new QueryArgument<StringGraphType> {Name = "uprn"},
+                    new QueryArgument<StringGraphType> {Name = "companiesHouseNumber"},
+                    new QueryArgument<StringGraphType> {Name = "charitiesCommissionNumber"},
+                    new QueryArgument<StringGraphType> {Name = "dfeNumber"},
+                    new QueryArgument<IntGraphType> {Name = "establishmentNumber"},
+                    new QueryArgument<IntGraphType> {Name = "previousEstablishmentNumber"},
+                }));
+
             Field<ListGraphType<LearningProviderType>>("learningProviders",
                 resolve: learningProvidersResolver.ResolveAsync,
                 arguments: new QueryArguments(
