235430416051490e208dfc70d4826e3884c97d4a
Simon Bull <simon.bull.84@googlemail.com>
2020-04-08 06:55:28 +0100

    Fix field selection for management groups

--- src/Dfe.Spi.GraphQlApi.Application/Resolvers/ManagementGroupsResolver.cs
+++ src/Dfe.Spi.GraphQlApi.Application/Resolvers/ManagementGroupsResolver.cs
@@ -134,12 +134,22 @@ namespace Dfe.Spi.GraphQlApi.Application.Resolvers
 
         private string[] GetRequestedFields<T>(ResolveFieldContext<T> context)
         {
-            var selections = context.FieldAst.SelectionSet.Selections.Select(x => ((Field) x).Name);
+            var defaultFields = new[] {"code"};
+            
+            var resultsField = (Field)context.FieldAst.SelectionSet.Selections
+                .SingleOrDefault(x=>((Field) x).Name.Equals("results", StringComparison.InvariantCultureIgnoreCase));
+            if (resultsField == null)
+            {
+                return defaultFields;
+            }
+            
+            var selections = resultsField.SelectionSet.Selections.Select(x => ((Field) x).Name);
             
             // Will need identifiers for resolving sub objects (such as census), so request them from backend
-            selections = selections.Concat(new[] {"code"}).Distinct();
-
-            return selections.ToArray();
+            return selections
+                .Concat(defaultFields)
+                .Distinct()
+                .ToArray();
         }
     }
 }
