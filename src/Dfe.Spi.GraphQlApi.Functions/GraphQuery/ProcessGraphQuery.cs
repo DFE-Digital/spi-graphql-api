dbbe17847ee1aa543eab4cbd4529476c429111a2
Andew Macdonald <andymac@nimbleapproach.com>
2022-01-25 12:00:43 +0000

    Adding verbos loging to help with diag

--- src/Dfe.Spi.GraphQlApi.Functions/GraphQuery/ProcessGraphQuery.cs
+++ src/Dfe.Spi.GraphQlApi.Functions/GraphQuery/ProcessGraphQuery.cs
@@ -39,20 +39,26 @@ namespace Dfe.Spi.GraphQlApi.Functions.GraphQuery
             HttpRequest req,
             CancellationToken cancellationToken)
         {
+            _logger.Info("Entering function call");
             var startTime = DateTime.Now;
 
             _contextManager.SetContext(req.Headers);
             _contextManager.SetInternalRequestId(Guid.NewGuid());
+            _logger.Info("Set context");
 
             string result;
             if (req.Method.Equals("GET") && req.Query.ContainsKey("schema"))
             {
+                _logger.Info("Sending schema via GET request");
                 result = new SchemaPrinter(_spiSchema).Print();
             }
             else
             {
+                _logger.Info("POST request detected, attempting graph QL query");
                 var graphRequest = await ExtractGraphRequestAsync(req);
+                _logger.Info($"Graph request extracted: {graphRequest}");
                 result = await _spiSchema.ExecuteAsync(graphRequest);
+                _logger.Info($"Graph result fetched: {result}");
             }
 
             var endTime = DateTime.Now;
